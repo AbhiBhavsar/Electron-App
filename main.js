6e5f0b069b3e10ef3ba8609cb27a5b5f5556eae8
Abhishek Bhavsar <abhi.n.bhavsar@gmail.com>
2020-09-04 16:57:47 +0530

    Added parent child window

--- main.js
+++ main.js
@@ -1,39 +1,50 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require("electron");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-
+let mainWindow, secondaryWindow;
 // Create a new BrowserWindow when `app` is ready
-function createWindow () {
+function createWindow() {
+	mainWindow = new BrowserWindow({
+		width: 500,
+		height: 800,
+		webPreferences: { nodeIntegration: true },
+		backgroundColor: "#2c92ef",
+	});
 
-  mainWindow = new BrowserWindow({
-    width: 1000, height: 800,
-    webPreferences: { nodeIntegration: true }
-  })
+	secondaryWindow = new BrowserWindow({
+		width: 300,
+		height: 300,
+		webPreferences: { nodeIntegration: true },
+		parent: mainWindow,
+		modal: true,
+		frame: false,
+	});
 
-  // Load index.html into the new BrowserWindow
-  mainWindow.loadFile('index.html')
+	// Load index.html into the new BrowserWindow
+	mainWindow.loadFile("index.html");
+	secondaryWindow.loadFile("secondary.html");
+	// mainWindow.loadURL("https://www.google.com");
 
-  // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+	// Open DevTools - Remove for PRODUCTION!
+	// mainWindow.webContents.openDevTools();
 
-  // Listen for window being closed
-  mainWindow.on('closed',  () => {
-    mainWindow = null
-  })
+	// Listen for window being closed
+	mainWindow.on("closed", () => {
+		mainWindow = null;
+	});
 }
 
 // Electron `app` is ready
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed - (Not macOS - Darwin)
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") app.quit();
+});
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
-app.on('activate', () => {
-  if (mainWindow === null) createWindow()
-})
+app.on("activate", () => {
+	if (mainWindow === null) createWindow();
+});
